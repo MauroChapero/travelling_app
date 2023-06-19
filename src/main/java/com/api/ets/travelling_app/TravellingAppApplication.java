bd55bbaeb60ac0c220d7f2ca79fb24dce8c4d94c
MauroChapero <chaperomauro1803@gmail.com>
2023-06-19 13:49:33 -0300

    Project Init

---
+++ src/main/java/com/api/ets/travelling_app/TravellingAppApplication.java
@@ -0,0 +1,13 @@
+package com.api.ets.travelling_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellingAppApplication.class, args);
+	}
+
+}
